967d8211cd0500969ccff859fc4f873f7241f995
Martí Ollé <martiollea@gmail.com>
2022-04-08 12:08:05 +0200

    Estructura de datos con json.

---
+++ main.js
@@ -0,0 +1,16 @@
+class Card {
+  constructor() {
+    this.number = number;
+    this.value = value;
+    this.visible = visible;
+  }
+}
+class Player {
+  constructor() {
+    this.firstname = firstname;
+    this.cards = cards;
+    this.points = points;
+  }
+}
+
+let deck = [];
